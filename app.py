e877a360fdc7cf1ae35d03b4939531fc3f8eb90a
Igor Ekimov <igor@lubabo.com>
2026-01-07 02:41:31 +0100

    Changes

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
-from flask import Flask, Response
+from flask import Flask
 from flasgger import Swagger
-
-from configuration import Configuration
-application_config = Configuration()
-
 from persons import persons
 from registration import registration
 from counter import counter
 
+from configuration import Configuration
+application_config = Configuration()
+
 app = Flask(__name__)
-swagger = Swagger(app, template={
-    "info": {
-        "title": "24swim.de",
-        "description": "Backend API used to work with 24h swimming events",
-        "version": "1.0.0"
-    }
-})
-app.config['SWAGGER'] = {
-    'title': 'My API',
-    'uiversion': 3,
-    'doc_dir': './api/',
-    'openapi': '3.0.2'
-}
+swagger = Swagger(app)
+
 app.register_blueprint(persons)
 app.register_blueprint(registration)
 app.register_blueprint(counter)
